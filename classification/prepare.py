fa1cef4b717576c5c4453515cb4248afa63959bc
Noah Melngailis <noah.melngailis@gmail.com>
2020-04-18 20:28:23 -0500

    prepare.py changes from earlier this week

--- classification/prepare.py
+++ classification/prepare.py
@@ -4,17 +4,16 @@ import sklearn.impute
 import sklearn.model_selection
 import sklearn.preprocessing
 
-def drop_columns_iris(df):
+def drop_columns(df):
     df.drop(columns = ['species_id', 'measurement_id'], inplace = True)
     return df
 
-def rename_columns_iris(df):
+def rename_columns(df):
     df.rename(columns = {'species_name':'species'}, inplace = True)
     return df
 
 def encode_species(train, test):
-    encoder = sklearn.preprocessing.OneHotEncoder()
-    encoder.fit(train[['species']])
+    encoder = sklearn.preprocessing.OneHotEncoder().fit(train[['species']])
     cols = ['embark_town_' + c for c in encoder.categories_[0]]
 
     m = encoder.transform(train[['species']]).todense()
@@ -29,13 +28,62 @@ def encode_species(train, test):
         pd.DataFrame(m, columns = cols, index=test.index)
     ], axis = 1).drop(columns='species')    
     
-    return train, test
+    return encoder, train, test
     
 
 def prep_iris(df):
-    df = drop_columns_iris(df)
-    df = rename_columns_iris(df)
+    df = drop_columns(df)
+    df = rename_columns(df)
     train, test = sklearn.model_selection.train_test_split(df, random_state=123, train_size = .8)
-    train, test = encode_species(train, test)
+    encoder, train, test = encode_species(train, test)
+    return encoder, train, test
+
+def drop_columns_titanic(df):
+    df.drop(columns = ['deck', 'embarked', 'class'], inplace = True)
+    return df
+
+def impute_embark_town(train, test):
+    train.embark_town = train.embark_town.fillna("Southampton")
+    test.embark_town = test.embark_town.fillna("Southampton")
     return train, test
+
+def encode_embark_town(train, test):
+    encoder = sklearn.preprocessing.OneHotEncoder().fit(train[['embark_town']])
+    
+    cols = ['embark_town_' + c for c in encoder.categories_[0]]
+
+    m = encoder.transform(train[['embark_town']]).todense()
+    train = pd.concat([
+        train,
+        pd.DataFrame(m, columns=cols, index=train.index)
+    ], axis=1).drop(columns='embark_town')
+    
+    m = encoder.transform(test[['embark_town']]).todense()
+    test = pd.concat([
+        test,
+        pd.DataFrame(m, columns=cols, index=test.index)
+    ], axis=1).drop(columns='embark_town')
+    return encoder, train, test
+
+def impute_age(train, test):
+    imputer = sklearn.impute.SimpleImputer(strategy='mean')
+    imputer.fit(train[['age']])
+    train.age = imputer.transform(train[['age']])
+    test.age = imputer.transform(test[['age']])
+    return train, test
+
+def scale_columns(train, test):
+    scaler = sklearn.preprocessing.MinMaxScaler()
+    train[['age','fare']] = scaler.fit_transform(train[['age','fare']])
+    test[['age','fare']] = scaler.transform(test[['age','fare']])
+    return scaler, train, test
+
+def prep_titanic(df):
+    df = drop_columns_titanic(df)
+    train, test = sklearn.model_selection.train_test_split(df, random_state=123, train_size=.8)
+    train, test = impute_embark_town(train, test)
+    encoder, train, test = encode_embark_town(train, test)
+    train, test = impute_age(train, test)
+    scaler, train, test = scale_columns(train, test)
+    return scaler, encoder, train, test
 
