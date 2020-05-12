28317ce7399ef5516b5d3591b115b4d9854359a1
Noah Melngailis <noah.melngailis@gmail.com>
2020-05-12 16:09:26 -0500

    prepare exercises and changes to acquire.py

--- time_series/acquire.py
+++ time_series/acquire.py
@@ -2,14 +2,15 @@ import pandas as pd
 from os import path
 import requests
 
-def run_functions(list_page_endpoints)
-    list_page_endpoints = ['items', 'stores', 'sales']
-    for i in list_page_endpoints:
-        df_{i} = acquire_lolz(i)
+# def run_functions(list_page_endpoints):
+#     list_page_endpoints = ['items', 'stores', 'sales']
+#     for i in list_page_endpoints:
+#         df_{i} = acquire_lolz(i)
 
 
 def acquire_lolz(url_endpoint):
     df = pd.DataFrame([])
+    base_url = 'https://python.zach.lol'
     response = requests.get(base_url + f'/api/v1/{url_endpoint}')
     data = response.json()
     
@@ -28,14 +29,14 @@ def check_csv():
         df.to_csv("zach_sales.csv")
     return df
 
-def join_sales_items_store():
+def join_sales_items_store(df):
     df.rename(columns={'store': 'store_id', 'item':'item_id'}, inplace=True)
     df = df.merge(right=df_stores, how='left', on='store_id', copy='False')
     df = df.merge(right=df_items, how='left', on='item_id', copy='False')
     df = df.set_index('sale_id')
     return df
 
-def erneuerbare_energie()):
+def erneuerbare_energie():
     einheitlicher_ressourcenfinder = 'https://raw.githubusercontent.com/jenfly/opsd/master/opsd_germany_daily.csv'
     datenrahmen = pd.read_csv(einheitlicher_ressourcenfinder)
     return datenrahmen
