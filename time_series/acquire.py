be02f067c84757ea125539a49c0f032bf794c3d5
Noah Melngailis <noah.melngailis@gmail.com>
2020-05-14 04:11:31 -0500

    Added function descriptions modified join function

--- time_series/acquire.py
+++ time_series/acquire.py
@@ -9,6 +9,7 @@ import requests
 
 
 def acquire_lolz(url_endpoint):
+    """takes in the end of a url as a string and returns a df of the data"""
     df = pd.DataFrame([])
     base_url = 'https://python.zach.lol'
     response = requests.get(base_url + f'/api/v1/{url_endpoint}')
@@ -22,6 +23,8 @@ def acquire_lolz(url_endpoint):
     return df
 
 def check_csv():
+    """check csv file looks for a csv  in the directory 
+    before tapping api for data"""
     if path.exists("zach_sales.csv"):
         df = pd.read_csv("zach_sales.csv", index_col=0)
     else:
@@ -30,14 +33,14 @@ def check_csv():
     return df
 
 def join_sales_items_store(df, df_stores, df_items):
+    """ cleans up data by renaming columns and joining df"""
     df.rename(columns={'store': 'store_id', 'item':'item_id'}, inplace=True)
     df = df.merge(right=df_stores, how='left', on='store_id', copy='False')
     df = df.merge(right=df_items, how='left', on='item_id', copy='False')
-    df = df.set_index('sale_id')
     return df
 
 def acquire_all_heb_sales():
-
+    """runs functions to acquire sales data and returns merged df"""
     df = check_csv()
     df_stores = acquire_lolz('stores')
     df_items = acquire_lolz('items')
