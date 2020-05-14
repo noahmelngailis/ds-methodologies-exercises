a4fa5fb9fde7da20e840e57cc9d7db60c150fc46
Noah Melngailis <noah.melngailis@gmail.com>
2020-05-14 03:56:07 -0500

    Cleaning up and creating master acquire function

--- time_series/acquire.py
+++ time_series/acquire.py
@@ -25,16 +25,29 @@ def check_csv():
     if path.exists("zach_sales.csv"):
         df = pd.read_csv("zach_sales.csv", index_col=0)
     else:
-        df = df_sales
+        df = acquire_lolz('sales')
         df.to_csv("zach_sales.csv")
     return df
 
-def join_sales_items_store(df):
+def join_sales_items_store(df, df_stores, df_items):
     df.rename(columns={'store': 'store_id', 'item':'item_id'}, inplace=True)
     df = df.merge(right=df_stores, how='left', on='store_id', copy='False')
     df = df.merge(right=df_items, how='left', on='item_id', copy='False')
     df = df.set_index('sale_id')
     return df
+
+def acquire_all_heb_sales():
+
+    df = check_csv()
+    df_stores = acquire_lolz('stores')
+    df_items = acquire_lolz('items')
+    df = join_sales_items_store(df, df_stores, df_items)
+
+    return df
+
+
+
+
 
 def erneuerbare_energie():
     einheitlicher_ressourcenfinder = 'https://raw.githubusercontent.com/jenfly/opsd/master/opsd_germany_daily.csv'
